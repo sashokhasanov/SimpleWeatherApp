739bfdc4191a3fc5003bc1543350d0c91bab1556
Aleksandr Khasanov <sashokhasanov@mail.ru>
2022-01-15 23:25:10 +0300

    minor refactoring

--- SimpleWeatherApp/WeatherInfo.swift
+++ SimpleWeatherApp/WeatherInfo.swift
@@ -24,7 +24,7 @@ struct WeatherInfo: Codable {
     }
 }
 
-// MARK: - Coord
+// MARK: - Coordinates
 struct Coordinates: Codable {
     let longtitude: Double?
     let latitude: Double?
@@ -74,40 +74,4 @@ struct Wind: Codable {
     let speed: Double?
     let deg: Int?
     let gust: Double?
-}
-
-// MARK: - Helper functions for creating encoders and decoders
-
-func newJSONDecoder() -> JSONDecoder {
-    let decoder = JSONDecoder()
-    if #available(iOS 10.0, OSX 10.12, tvOS 10.0, watchOS 3.0, *) {
-        decoder.dateDecodingStrategy = .iso8601
-    }
-    return decoder
-}
-
-func newJSONEncoder() -> JSONEncoder {
-    let encoder = JSONEncoder()
-    if #available(iOS 10.0, OSX 10.12, tvOS 10.0, watchOS 3.0, *) {
-        encoder.dateEncodingStrategy = .iso8601
-    }
-    return encoder
-}
-
-// MARK: - URLSession response handlers
-
-extension URLSession {
-    fileprivate func codableTask<T: Codable>(with url: URL, completionHandler: @escaping (T?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
-        return self.dataTask(with: url) { data, response, error in
-            guard let data = data, error == nil else {
-                completionHandler(nil, response, error)
-                return
-            }
-            completionHandler(try? newJSONDecoder().decode(T.self, from: data), response, nil)
-        }
-    }
-
-    func weatherInfoTask(with url: URL, completionHandler: @escaping (WeatherInfo?, URLResponse?, Error?) -> Void) -> URLSessionDataTask {
-        return self.codableTask(with: url, completionHandler: completionHandler)
-    }
 }
