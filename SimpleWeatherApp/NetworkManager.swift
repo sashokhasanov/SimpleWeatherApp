de157ecc52f7cbe66e5ce2598bf6c0342eff5648
Aleksandr Khasanov <sashokhasanov@mail.ru>
2022-01-15 23:17:38 +0300

    first stable version

--- SimpleWeatherApp/NetworkManager.swift
+++ SimpleWeatherApp/NetworkManager.swift
@@ -6,14 +6,59 @@
 //
 
 import Foundation
+import UIKit
 
 class NetworkManager {
     
     static let shared = NetworkManager()
     
+    private let apiKey = "e44c551160dc50f8423bc7d7db2805a5"
+    
     private init() {}
     
-    func fetchWeatherData() {
+    func fetchWeatherData(completionHandler: @escaping (WeatherInfo?) -> Void) {
+        guard let weatherUrl = makeQueryUrl() else { return }
         
+        URLSession.shared.dataTask(with: weatherUrl) { data, _, _ in
+            guard let data = data else { return }
+            sleep(5)
+            let weatherInfo = try? JSONDecoder().decode(WeatherInfo.self, from: data)
+            completionHandler(weatherInfo)
+        }.resume()
+    }
+    
+    func fetchWeatherIcon(with id: String, completionHandler: @escaping (UIImage?) -> Void) {
+        guard let iconUrl = makeIconUrl(for: id) else { return }
+        
+        URLSession.shared.dataTask(with: iconUrl) { data, _, _ in
+            guard let data = data else { return }
+            sleep(2)
+            let icon = UIImage(data: data)
+            completionHandler(icon)
+        }.resume()
+    }
+    
+    private func makeQueryUrl() -> URL? {
+        var components = URLComponents()
+        components.scheme = "https"
+        components.host = "api.openweathermap.org"
+        components.path = "/data/2.5/weather"
+        components.queryItems = [
+            URLQueryItem(name: "q", value: "Moscow"),
+            URLQueryItem(name: "units", value: "metric"),
+            URLQueryItem(name: "lang", value: "ru"),
+            URLQueryItem(name: "appid", value: apiKey)
+        ]
+        
+        return components.url
+    }
+    
+    private func makeIconUrl(for iconId: String) -> URL? {
+        var components = URLComponents()
+        components.scheme = "https"
+        components.host = "openweathermap.org"
+        components.path = "/img/wn/\(iconId)@2x.png"
+
+        return components.url
     }
 }
--- SimpleWeatherApp/ViewController.swift
+++ SimpleWeatherApp/ViewController.swift
@@ -9,41 +9,70 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    // MARK: - IBOutlets
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var feelsLikeLabel: UILabel!
+    @IBOutlet weak var iconView: UIImageView!
+    @IBOutlet weak var mainInfoView: UIStackView!
+    
+    // MARK: - Override methods
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        updateWeather()
     }
+    
+    // MARK: - Private methos
+    private func updateWeather() {
+        startAnimate(view: mainInfoView)
 
+        NetworkManager.shared.fetchWeatherData() { weatherInfo in
 
-    @IBAction func buttonPressed(_ sender: UIButton) {
+            DispatchQueue.main.async {
+                self.stopAnimateView(view: self.mainInfoView)
+            }
         
-        guard let url = makeQueryUrl() else { return }
-        
-        
-        
-        URLSession.shared.weatherInfoTask(with: url) { weatherInfo, response, error in
             guard let weatherInfo = weatherInfo else { return }
-            
-            print(weatherInfo.name)
-            
-        }.resume()
+
+            DispatchQueue.main.async {
+                if let iconId = weatherInfo.weather?[0].icon {
+                    self.updateWeaterIcon(for: iconId)
+                }
+                self.temperatureLabel.text = String(format: "%0.f°C", weatherInfo.main?.temp ?? 0)
+                self.feelsLikeLabel.text = String(format: "Ощущается как %0.f°C", weatherInfo.main?.feelsLike ?? 0)
+
+                let description = weatherInfo.weather?[0].weatherDescription ?? ""
+                self.descriptionLabel.text = description.prefix(1).capitalized + description.dropFirst()
+            }
+        }
     }
     
-    private let apiKey = "e44c551160dc50f8423bc7d7db2805a5"
-    
-    func makeQueryUrl() -> URL? {
-        var components = URLComponents()
-        components.scheme = "https"
-        components.host = "api.openweathermap.org"
-        components.path = "/data/2.5/weather"
-        components.queryItems = [
-            URLQueryItem(name: "q", value: "Moscow"),
-            URLQueryItem(name: "units", value: "metric"),
-            URLQueryItem(name: "lang", value: "ru"),
-            URLQueryItem(name: "appid", value: apiKey)
-        ]
+    private func updateWeaterIcon(for iconId: String) {
         
-        return components.url
+        startAnimate(view: iconView)
+        
+        NetworkManager.shared.fetchWeatherIcon(with: iconId) { icon in
+            DispatchQueue.main.async {
+                self.stopAnimateView(view: self.iconView)
+            }
+            
+            guard let icon = icon else { return }
+
+            DispatchQueue.main.async {
+                self.iconView.image = icon
+            }
+        }
+    }
+    
+    private func startAnimate(view: UIView) {
+        UIView.animate(withDuration: 0.85, delay: 0, options: [.repeat, .autoreverse] ) {
+            view.alpha = 0
+        }
+    }
+    
+    private func stopAnimateView(view: UIView) {
+        view.layer.removeAllAnimations()
+        view.alpha = 1
     }
 }
 
